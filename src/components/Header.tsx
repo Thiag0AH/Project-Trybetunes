bd5d3b5852383f58e2be543d452bac0bc56e7ae4
Thiago Antonio Hermont <thiagoantoniohermont@gmail.com>
2023-09-02 03:39:39 -0300

    Requisito 4

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import { UserType } from '../types';
+
+function Header() {
+  const [carregando, setCarregando] = useState(true);
+  const [user, setUser] = useState<UserType>();
+  const [userDysplay, setUserDysplay] = useState<string>();
+  useEffect(() => {
+    const userHandler = async () => {
+      const username = await getUser();
+      setUser(username);
+    };
+    const getName = () => {
+      if (typeof (user) !== 'undefined') {
+        setUserDysplay(user.name);
+        return true;
+      }
+      return false;
+    };
+    userHandler();
+    if (getName()) {
+      setCarregando(false);
+    }
+  }, [user]);
+  if (carregando) {
+    return <h1>Carregando...</h1>;
+  }
+  return (
+    <>
+      <header data-testid="header-component" />
+      <Link to="/search" data-testid="link-to-search" />
+      <Link to="/favorites" data-testid="link-to-favorites" />
+      <Link to="/profile" data-testid="link-to-profile" />
+      <p data-testid="header-user-name">{ userDysplay }</p>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+function Layout() {
+  return (
+    <div data-testid="outlet-mockada">
+      <Header />
+    </div>
+  );
+}
+
+export default Layout;
